d6ea8ed4b9ebcc676d73c067e5b4f7d28cf1637a
Beto Maia <adalberto.maia@gmail.com>
2026-06-19 16:58:50 -0300

    refactor: move Google Fonts import from CSS to Nuxt configuration link tags for optimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -108,6 +108,9 @@ export default defineNuxtConfig({
         { name: 'twitter:image', content: 'https://www.valepcd.com.br/images/logo-vale-pcd-seo.webp' }
       ],
       link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&display=swap' },
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
       ]
     }
