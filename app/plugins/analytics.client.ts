54b6de6ad030f3ac23c081c478fb054a30a4353a
Beto Maia <adalberto.maia@gmail.com>
2026-07-02 19:44:49 -0300

    refactor: simplify gtag page_view event trigger by removing conditional logs and redundant checks

--- app/plugins/analytics.client.ts
+++ app/plugins/analytics.client.ts
@@ -14,17 +14,12 @@ export default defineNuxtPlugin((nuxtApp) => {
   router.afterEach((to) => {
     // We use nextTick to ensure the DOM (specifically <title>) has been updated by Nuxt before triggering the page_view
     nextTick(() => {
-      if (typeof window !== 'undefined') {
-        if (window.gtag) {
-          console.log('[Analytics] Triggering page_view for:', to.fullPath)
-          window.gtag('event', 'page_view', {
-            page_title: document.title,
-            page_path: to.fullPath,
-            page_location: window.location.href
-          })
-        } else {
-          console.warn('[Analytics] window.gtag is not defined!')
-        }
+      if (typeof window !== 'undefined' && window.gtag) {
+        window.gtag('event', 'page_view', {
+          page_title: document.title,
+          page_path: to.fullPath,
+          page_location: window.location.href
+        })
       }
     })
   })
