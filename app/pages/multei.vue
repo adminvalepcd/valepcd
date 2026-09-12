f34b0252661ad071aa0dd84a6a83f81f2b71171c
Beto Maia <adalberto.maia@gmail.com>
2026-09-12 13:05:18 -0300

    chore(multei): update Google Apps Script deployment URL to AKfycbxO2pUAcnnZ4lnsWUBcSS7__SpkMo0s4aJGEB-uw_GgCI-luQu1WOroWl5A2XhW_Lww

--- app/pages/multei.vue
+++ app/pages/multei.vue
@@ -110,7 +110,7 @@ const config = useRuntimeConfig();
 const appsScriptUrl = ref(
   (config.public.appsScriptUrl) || 
   (process.env.VITE_APPS_SCRIPT_URL) || 
-  'https://script.google.com/macros/s/AKfycbz7Q9F9BxLKwz43wDmGT77fVWMCsPsK9BBs1RoKisSxfxSi23KpU37tnLtLqEmdPG7D/exec'
+  'https://script.google.com/macros/s/AKfycbxO2pUAcnnZ4lnsWUBcSS7__SpkMo0s4aJGEB-uw_GgCI-luQu1WOroWl5A2XhW_Lww/exec'
 );
 
 const isCreateModalOpen = ref(false);
